7ca2a351c3da0e39ded0270d2beeb1dcd82ad2d6
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-16 19:53:31 -0300

    #condiçóes aninhadas

---
+++ aula1/teste.py
@@ -0,0 +1,40 @@
+#condiçóes aninhadas
+# Verifica se um número é positivo, negativo ou zero
+
+carro = 'ferrari'  
+nome = str(input('para qual lado ele deve ir? '))
+
+#CONDIÇAO
+if nome == carro:
+    print('certo')
+else:
+    print('ERRO, o carro citado não é uma {} '.format(carro))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
