d78193da88602827dd4b775fb915f3484efede76
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-24 10:46:37 -0300

    Update g-pagamentos.py

--- aula9/g-pagamentos.py
+++ aula9/g-pagamentos.py
@@ -1,14 +1,32 @@
 # GERENCIADOR DE PAGAMENTOS
+#PERGUNTAS
+print("{:=^40}".format("RDPLAY STORE"))#COLOCANDO ESPAÇOS CENTRALIZANDO AO MEIO
+preço = float(input("VALOR DAS COMPRAS: R$"))
 
+print(''' FORMAS DE PAGAMENTO 
+[ 1 ] á vista dinheiro / cheque
+[ 2 ] á vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opçao = int(input('Qual é a opção? '))
 
-
-
-
-
-
-
-
-
+if opçao == 1:
+    total = preço - (preço * 10 / 100)
+elif opçao == 2:
+    total = preço - (preço * 5 / 100)
+elif opçao == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de {:.2f} SEM JUROS'.format(parcela))
+elif opçao == 4:
+    total = preço + (preço * 20 /100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(totparc, parcela))
+else:
+    total = 0
+    print('OPÇÃO INVALIDA TENTE NOVAMENTE!')
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço , total))
 
 
 
