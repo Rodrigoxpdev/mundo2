5970d89761044181252a900409a0ce5143d5c29f
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-29 23:12:20 -0300

    Create tab.py

---
+++ aula15/tab.py
@@ -0,0 +1,53 @@
+#TABUADA V.2.0
+#IMPORTAÇAO
+
+
+
+num = int(input('DIGITE UM NUMERO PARA VER A TABUADA: '))
+
+#SOMA
+for c in range(1, 11):# VAI DE 1 ATE 10
+    print('{} x {:2} = {}'.format(num , c , num*c))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
