936db897e46031453cbf10e6d4ed6c242a60e677
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-30 16:26:53 -0300

    Create pares.py

---
+++ aula16/pares.py
@@ -0,0 +1,18 @@
+soma = 0 
+cont = 0
+
+for c in range(1,7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    if num % 2 == 0: #SE O NUMERO DIGITADO FOR DIVIDIDO POR 2 FOR IGUAL A 0 
+        soma += num
+        cont += 1
+print('voce informou {} numeros PARES! e a soma foi {} '.format(cont, soma))
+
+
+
+
+
+
+
+
+
