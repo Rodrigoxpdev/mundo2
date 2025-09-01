98edea811784fc17a23d793044d3707ce969b24d
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-31 23:20:25 -0300

    Create numerosPrimos.py

---
+++ aula18/numerosPrimos.py
@@ -0,0 +1,26 @@
+
+num = int(input('DIGITE UM NÚMERO: '))
+tot = 0 #TOTAL
+
+for c in range(1, num + 1):
+    if num % c == 0: 
+        print('\033[33m',end=' ') 
+        tot += 1 
+    else:
+        print('\033[31m',end=' ')
+    print('{} '.format(c),end=' ')
+print('O NÚMERO {} FOI DIVISIVEL {} VEZES'.format(num, tot))
+if tot == 2:
+    print('E POR ISSO ELE É PRIMO!')
+else:
+    print('E POR ISSO ELE NÃO É PRIMO!')
+
+
+
+
+
+
+
+
+
+
