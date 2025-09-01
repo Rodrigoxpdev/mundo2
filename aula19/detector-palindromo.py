8edcc21757a4ce56c6ae1e55c5fc7f1c77c372ef
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-08-31 23:54:55 -0300

    Create detector-palindromo.py

---
+++ aula19/detector-palindromo.py
@@ -0,0 +1,50 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+# -----------------------------------------------------
+
+palavras = frase.split()
+junto = "".join(palavras)
+print('voce digitou a frase {} '.format(junto))
+# -----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
