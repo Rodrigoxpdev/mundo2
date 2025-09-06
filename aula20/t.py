b65c2437cd1ae0db3913cb86d878a16b14c0dde1
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-06 16:50:24 -0300

    Create t.py

---
+++ aula20/t.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+pergunta1 = str(input('Qual seu nome? ')).strip().upper()
+print('PENSANDO..')
+
+sleep(1)
+print('--'*20)
+print('Seja bem vindo {} ao seu painel de Acesso'.format(pergunta1))
+print('--'*20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
