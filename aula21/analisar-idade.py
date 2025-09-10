e72821e1b77e45b68ed47a159597a45779770b23
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-09 23:05:59 -0300

    .

---
+++ aula21/analisar-idade.py
@@ -0,0 +1,53 @@
+# analisador de idade 
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+
+for pessoa in range(1, 8):
+    nasc = int(input('Em que ano a {} passoa nasceu? '.format(pessoa)))
+    idade= atual - nasc
+    if idade >= 21:
+        totmaior +=1
+    else:
+        totmenor+=1
+print('Ao todo tivemos {} pessoas maiores de idade! '.format(totmaior))
+print('E também tivemos {} pessoas menores de idade! '.format(totmenor))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
