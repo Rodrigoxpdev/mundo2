ede9bea20c11527411e1cedfd02096e4eae3a4c2
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-10 23:09:39 -0300

    Update estrutura-de-repetiçao.py

--- aula24/estrutura-de-repetiçao.py
+++ aula24/estrutura-de-repetiçao.py
@@ -16,16 +16,26 @@
 
 
 #exemplo 3
-r = 'S' #RESPOSTA
+# r = 'S' #RESPOSTA
 
-while r == 'S':
+# while r == 'S':
+#     n = int(input('Digite um valor: '))
+#     r = str(input('Quer continuar? [S/N] ')).upper() 
+# print('fim')
+
+
+#exemplo 4
+n =1
+par = impar = 0
+
+while n != 0:
     n = int(input('Digite um valor: '))
-    r = str(input('Quer continuar? [S/N] ')).upper() 
-print('fim')
-
-
-
-
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('voce digitou {} numeros pares e {} numeros impares'.format(par, impar))
 
 
 
