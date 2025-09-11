a8159cb83ce5df036e438dd0d972915ef9bb6c61
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-10 22:57:34 -0300

    .

---
+++ aula24/estrutura-de-repetiçao.py
@@ -0,0 +1,60 @@
+# c = 1 #contador começando em 1
+
+# while c < 10: #contador menor que 10
+#     print(c)
+#     c = c +1 
+# print('fim')
+
+
+
+#EXEMPLO 2
+# n =1 #se for diferente de 0 faça, se for 0 pare o programa
+
+# while n != 0:
+#     n = int(input('Digite um valor: '))
+# print('fim')
+
+
+#exemplo 3
+r = 'S' #RESPOSTA
+
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper() 
+print('fim')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
