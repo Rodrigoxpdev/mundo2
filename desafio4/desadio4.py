0bf2e39959709bc20f66742742f79a511000d0b5
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-13 17:32:31 -0300

    Update desadio4.py

--- desafio4/desadio4.py
+++ desafio4/desadio4.py
@@ -1,19 +1,17 @@
-# AREA DE TESTE
+# CALCULO DE FATORIAL
 from math import factorial
-
 
 n = int(input('Digite um numero para calcular seu fatorial: '))
 f = factorial(n)
 c = n
 f = 1
 
-
-
 while c > 0:
     print('{}'.format(c), end=' ')
     print(' x ' if c > 1 else '=', end='')
+    f*=c
     c -= 1
-# print('o fatorial de: {} é {}'.format(n , f))
+print(' {}'.format(f))
 
 
 
