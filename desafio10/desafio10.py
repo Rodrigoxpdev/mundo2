b1f2c3cd3000e32066562b5f988d654fed24074d
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-18 22:49:33 -0300

    Update desafio10.py

--- desafio10/desafio10.py
+++ desafio10/desafio10.py
@@ -1,9 +1,15 @@
+#TABUADA DE MULTIPLICAÇÂO 
 
-
-
-
-
-
+#EXEMPLO
+while True:
+    num =  int(input('Quer ver a tabuada de qual valor? '))
+    print('--'*20)
+    if num < 0:
+        break #PARAR
+    for c in range(1, 11):
+        print(f'{num} x {c} = {num*c}')
+    print('--'*20)
+print('Programa encerrado!')
 
 
 
