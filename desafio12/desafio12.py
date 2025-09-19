537b0c0179b64fec46ef081833cd7484d0bb94b7
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-18 23:38:56 -0300

    Create desafio12.py

---
+++ desafio12/desafio12.py
@@ -0,0 +1,55 @@
+#ANALISE DE DADOS DO GRUPO
+
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+        if resp == 'N':
+            break
+print('acabou')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
