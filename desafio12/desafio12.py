8f256d9c85baed34c03e5962aab48468d1e2e441
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-18 23:46:59 -0300

    Update desafio12.py

--- desafio12/desafio12.py
+++ desafio12/desafio12.py
@@ -5,10 +5,12 @@ while True:
     sexo = ' '
     while sexo not in 'MF':
         sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+
+
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-        if resp == 'N':
+    if resp == 'N':
             break
 print('acabou')
 
