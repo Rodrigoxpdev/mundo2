d5e3fdc259094d9b75b9ad5ca1a0f19bda4603cf
Rodrigoxpdev <rodrigosantos2190r@gmail.com>
2025-09-19 23:03:22 -0300

    .

--- desafio12/desafio12.py
+++ desafio12/desafio12.py
@@ -1,28 +1,47 @@
 #ANALISE DE DADOS DO GRUPO
+# tot18 = totH = tot20 =  0
 
-while True:
-    idade = int(input('Idade: '))
-    sexo = ' '
-    while sexo not in 'MF':
-        sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+# while True: #loop infinito TRUE
+#     idade = int(input('Idade: '))
+#     sexo = ' '
+#     while sexo not in 'MF':
+#         sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+#     if idade >= 18:
+#         tot18 += 1
+#     if sexo == 'M':
+#         totH += 1
+#     if sexo == 'F' and idade < 20:
+#         tot20 += 1
+#     resp = ' '
+#     while resp not in 'SN':
+#         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+#     if resp == 'N':
+#             break
+# print(f'Total de pessoas com mais de 18 anos: {tot18}')
+# print(f'Ao todo temos {totH} homens cadastrados ')
+# print(f'E temos {tot20} mulheres com menos de 20 anos ')
 
 
-    resp = ' '
-    while resp not in 'SN':
-        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    if resp == 'N':
-            break
-print('acabou')
 
 
 
 
-
-
-
-
-
-
+#fazendo teste
+# while True:
+#     idade = int(input('Idade: '))
+        
+#     if idade >= 18:
+#         print(f'Você é Adulto sua idade é {idade} anos.')
+#     else:
+#         print(f'Você é menor de idade')
+        
+    
+#     resp = ' '  
+#     while resp not in 'SN':
+#         resp = str(input('Quer sair [S/N] ? ')).strip().upper()[0]
+#     if resp == 'N':
+#         break
+# print('Fim do programa')
 
 
 
